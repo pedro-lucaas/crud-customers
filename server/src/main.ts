20304be7d6fe201b78654a768699fa4e1685ec03
pedro-lucaas <pedro.plso1234@gmail.com>
2023-05-05 12:01:59 -0300

    feat: setup main file

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  try {
+    const app = await NestFactory.create(AppModule, {
+      cors: {
+        origin: '*',
+      },
+    });
+
+    await app.listen(3000);
+
+    console.log(`Application is running on: ${await app.getUrl()}`);
+  } catch (error) {
+    throw new Error(error);
+  }
 }
 bootstrap();
