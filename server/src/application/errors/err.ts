6d28a90c5c5d5ada5bf49a5336c012df4dff5249
pedro-lucaas <pedro.plso1234@gmail.com>
2023-05-05 12:25:46 -0300

    refactor: customized err

--- server/src/application/errors/err.ts
+++ server/src/application/errors/err.ts
@@ -1,12 +1,8 @@
 export class Err extends Error {
-  private _status: number;
+  public readonly status: number;
 
   constructor(message, status) {
     super(message);
-    this._status = status;
-  }
-
-  get status() {
-    return this._status;
+    this.status = status;
   }
 }
