45bf33f44d2ea1e60c4798b569d798dde71950ab
pedro-lucaas <pedro.plso1234@gmail.com>
2023-05-05 16:00:51 -0300

    refactor: change databse to use "in memory db"  on test environment

--- server/src/infra/database/database.module.ts
+++ server/src/infra/database/database.module.ts
@@ -2,17 +2,15 @@ import { Module } from "@nestjs/common";
 import { FirebaseService } from "./firebase/firebase.service";
 import { CustomersRepository } from "@application/repositories/customers-repository";
 import { FirebaseCustomersRepository } from "./firebase/repositories/firebase-customers-repository";
-
+import { InMemoryCustomersRepository } from "@test/repositories/in-memory-customers-repository";
 
 @Module({
-  imports: [
-    DatabaseModule,
-  ],
+  imports: [],
   providers: [
     FirebaseService,
     {
       provide: CustomersRepository,
-      useClass: FirebaseCustomersRepository,
+      useClass: process.env.NODE_ENV === 'test' ? InMemoryCustomersRepository : FirebaseCustomersRepository,
     }
   ],
   exports: [
