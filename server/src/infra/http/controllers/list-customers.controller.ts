7a56c747c7478a5f1fef5225928548ce3c62b402
pedro-lucaas <pedro.plso1234@gmail.com>
2023-05-11 18:42:14 -0300

    feat: create view models to display data on server

--- server/src/infra/http/controllers/list-customers.controller.ts
+++ server/src/infra/http/controllers/list-customers.controller.ts
@@ -1,5 +1,6 @@
 import { ListCustomersUseCase } from "@application/use-cases/list-customers-use-case";
-import { Controller, Get, HttpException, Query } from "@nestjs/common";
+import { Controller, Get, HttpException, ParseIntPipe, Query } from "@nestjs/common";
+import { CustomerViewModel } from "../view-models/customers-view-model";
 
 @Controller('customers')
 export class ListCustomersController {
@@ -8,9 +9,14 @@ export class ListCustomersController {
   ) { }
 
   @Get()
-  async handle(@Query('page') page: number) {
+  async handle(@Query('page', new ParseIntPipe()) page: number) {
     try {
-      return await this.listCustomersUseCase.execute(page);
+      const { customers } = await this.listCustomersUseCase.execute(page);
+
+      return {
+        ...customers,
+        items: customers.items.map(CustomerViewModel.toHttp)
+      };
     } catch (error) {
       throw new HttpException(error.message, error.status);
     }
